6720caca1bd440ac785aff0ffb7d1b7880bb2bf1
YosukeOHTA <deaufe@gmail.com>
2024-07-02 06:06:06 +0900

    adding page4

---
+++ pages/page_4.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.header('Plot map')
+df3 = pd.DataFrame(
+    np.random.rand(100, 2)/[50, 50]+ [35.69, 139.70],
+    columns=['lat', 'lon',]
+)
+st.map(df3)
